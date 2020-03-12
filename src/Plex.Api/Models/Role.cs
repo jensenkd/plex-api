a3501de0a11f37e2f9430b425d8562c435a28ada
Kevin Jensen <kevin@kineticmedia.net>
2020-03-12 09:43:09 -0500

    added rolename, id to role object

--- src/Plex.Api/Models/Role.cs
+++ src/Plex.Api/Models/Role.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel;
 using System.Text.Json.Serialization;
+using Plex.Api.Helpers;
 
 namespace Plex.Api.Models
 {
     public class Role
     {
+        [JsonPropertyName("id")]
+        [JsonConverter(typeof(IntValueConverter))]
+        public int Id { get; set; }
+
+        [JsonPropertyName("role")]
+        public string RoleName { get; set; }
+        
         [JsonPropertyName("tag")]
         public string Tag { get; set; }
     }
