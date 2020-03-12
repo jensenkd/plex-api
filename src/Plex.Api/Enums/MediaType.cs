1004c2e44d3979508d1bd4bb684a4871029504e3
Kevin Jensen <kevin@kineticmedia.net>
2020-03-12 18:20:39 -0500

    added enum for media type

---
+++ src/Plex.Api/Enums/MediaType.cs
@@ -0,0 +1,21 @@
+namespace Plex.Api.Enums
+{
+    public enum SessionMediaType
+    {
+        Movie,
+        Show,
+        Season,
+        Episode,
+        Trailer,
+        Comic,
+        Person,
+        Artist,
+        Album,
+        Track,
+        PhotoAlbum,
+        Picture,
+        Photo,
+        Clip,
+        PlayListItem
+    }
+}
