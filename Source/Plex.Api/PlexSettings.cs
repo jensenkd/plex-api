7cde812c889c11459ff2d4ac2c37027af8f42286
Kevin Jensen <kevin@kineticmedia.net>
2021-02-26 16:11:08 -0600

    updated documentation

--- Source/Plex.Api/PlexSettings.cs
+++ Source/Plex.Api/PlexSettings.cs
@@ -30,48 +30,48 @@ namespace Plex.Api
     public class PlexServers
     {
         /// <summary>
-        ///
+        /// Plex Server Name
         /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        ///
+        /// Plex Server Auth Token
         /// </summary>
         public string PlexAuthToken { get; set; }
 
         /// <summary>
-        ///
+        /// Plex Machine Identifier
         /// </summary>
         public string MachineIdentifier { get; set; }
 
         /// <summary>
-        ///
+        /// Plex Episode Batch Size
         /// </summary>
         public int EpisodeBatchSize { get; set; }
 
         /// <summary>
-        ///
+        /// Plex Selected Libraries
         /// </summary>
         public List<PlexSelectedLibraries> PlexSelectedLibraries { get; set; } = new List<PlexSelectedLibraries>();
     }
 
     /// <summary>
-    ///
+    /// Plex Selected Library
     /// </summary>
     public class PlexSelectedLibraries
     {
         /// <summary>
-        ///
+        /// Plex Library Key
         /// </summary>
         public int Key { get; set; }
 
         /// <summary>
-        ///
+        /// Plex Library Title
         /// </summary>
         public string Title { get; set; } // Name is for display purposes
 
         /// <summary>
-        ///
+        /// Plex Library Enabled?
         /// </summary>
         public bool Enabled { get; set; }
     }
